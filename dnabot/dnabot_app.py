ae398bf4a97f8d64ba05e3980fd0bbaa7cb0a389
LiamHallett <LiamHallett@users.noreply.github.com>
2021-03-01 01:38:39 +0000

    updated mplates, handle_2_columns, comments
    mplates and handle_2_columns made less verbose. Additional comments made throughout code.

--- dnabot/dnabot_app.py
+++ dnabot/dnabot_app.py
@@ -6,7 +6,7 @@ Created on Thu Apr 11 14:26:07 2019
 """
 
 # python dnabot\dnabot_app.py --construct_path \\icnas1.cc.ic.ac.uk\ljh119\GitHub\DNA-BOT\examples\construct_csvs\storch_et_al_cons\storch_et_al_cons.csv --source_paths \\icnas1.cc.ic.ac.uk\ljh119\GitHub\DNA-BOT\examples\part_linker_csvs\BIOLEGIO_BASIC_STD_SET.csv \\icnas1.cc.ic.ac.uk\ljh119\GitHub\DNA-BOT\examples\part_linker_csvs\part_plate_2_230419.csv
-# python3 dnabot/dnabot_app.py --construct_path /Users/liamhallett/Documents/GitHub/DNA-BOT/examples/construct_csvs/storch_et_al_cons/storch_et_al_cons.csv --source_paths /Users/liamhallett/Documents/GitHub/DNA-BOT/examples/part_linker_csvs/BIOLEGIO_BASIC_STD_SET.csv /Users/liamhallett/Documents/GitHub/DNA-BOT/examples/part_linker_csvs/parts_linkers_temp.csv
+# python3 dnabot/dnabot_app.py --construct_path /Users/liamhallett/Documents/GitHub/DNA-BOT/examples/construct_csvs/storch_et_al_cons/storch_et_al_cons.csv --source_paths /Users/liamhallett/Documents/GitHub/DNA-BOT/examples/part_linker_csvs/BIOLEGIO_BASIC_STD_SET.csv /Users/liamhallett/Documents/GitHub/DNA-BOT/examples/part_linker_csvs/part_plate_2_230419.csv
 
 print("\nINITIALISING>>>")
 
@@ -55,7 +55,7 @@ MAX_FINAL_ASSEMBLY_TIPRACKS = 7
 SPOTTING_VOLS_DICT = {2: 5, 3: 5, 4: 5, 5: 5, 6: 5, 7: 5}
 
 # Constant lists
-SOURCE_DECK_POS = ['2', '5', '8', '7', '10', '11'] # I guess this adds the functionality of having more source plates?
+SOURCE_DECK_POS = ['2', '5', '8', '7', '10', '11'] # deck positions available for source plates (only pos 2 and 5 are normally used)
 
 
 def __cli():
@@ -185,16 +185,16 @@ def main():
 
     # Process input csv files
     print('Processing input csv files...')
-    constructs_list = generate_constructs_list(construct_path) # returns a list of pandas dfs, each containing the clip reactions required for a given construct
-    clips_df = generate_clips_df(constructs_list)
-    # sources_dict = generate_sources_dict(sources_paths)
+    constructs_list = generate_constructs_list(construct_path)  # returns a list of pandas dfs, each containing the clip reactions required for a given construct
+    clips_df = generate_clips_df(constructs_list)               # generates df of unique clips, numbers required of each, and mag well location
+    sources_dict = generate_sources_dict(sources_paths)         # generates dict for sources with well location, conc, and pos location
 
-#     # calculate OT2 script variables
-#     print('Calculating OT-2 variables...')
-#     clips_dict = generate_clips_dict(clips_df, sources_dict)
-#     magbead_sample_number = clips_df['number'].sum()
-#     final_assembly_dict = generate_final_assembly_dict(constructs_list,
-#                                                        clips_df)
+    # calculate OT2 script variables
+    print('Calculating OT-2 variables...')
+    clips_dict = generate_clips_dict(clips_df, sources_dict)
+    magbead_sample_number = clips_df['number'].sum()            # total number of mag bead purifications required = 
+    final_assembly_dict = generate_final_assembly_dict(constructs_list,
+                                                       clips_df)
 #     final_assembly_tipracks = calculate_final_assembly_tipracks(
 #         final_assembly_dict)
 #     spotting_tuples = generate_spotting_tuples(constructs_list,
@@ -275,7 +275,7 @@ def generate_constructs_list(path):
                 else:
                     suffix_linker = interogate_linker(construct[i + 1])
                     clips_info['suffixes'].append(suffix_linker)    # add the suffix linker to the clips_info dict
-        return pd.DataFrame.from_dict(clips_info)   # converts the dictionary of parts to a pd.DataFrame
+        return pd.DataFrame.from_dict(clips_info)                   # converts the dictionary of parts to a pd.DataFrame
 
     constructs_list = []
     with open(path, 'r') as csvfile: # opens path as csvfile
@@ -302,7 +302,7 @@ def generate_clips_df(constructs_list):
     """
     merged_construct_dfs = pd.concat(constructs_list, ignore_index=True) # converts list of dfs into one large df
     unique_clips_df = merged_construct_dfs.drop_duplicates()    # drop duplicates
-    unique_clips_df = unique_clips_df.reset_index(drop=True)    # reset index
+    unique_clips_df = unique_clips_df.reset_index(drop=True)    # reset index (this creates a df of all unique clips)
     clips_df = unique_clips_df.copy() # makes a copy which is disconnected to the original
 
     # Error
@@ -311,118 +311,118 @@ def generate_clips_df(constructs_list):
             'Number of CLIP reactions exceeds 48. Reduce number of constructs in construct.csv.')
 
     # Count number of each CLIP reaction
-    clip_count = np.zeros(len(clips_df.index))
-    # for i, unique_clip in unique_clips_df.iterrows():
-    #     for _, clip in merged_construct_dfs.iterrows():
-    #         if unique_clip.equals(clip):
-    #             clip_count[i] = clip_count[i] + 1
-    # clip_count = clip_count // FINAL_ASSEMBLIES_PER_CLIP + 1
-    # clips_df['number'] = [int(i) for i in clip_count.tolist()]
-    print(clips_df)
+    clip_count = np.zeros(len(clips_df.index))              # creates an empty array the for each unique clip
+    for i, unique_clip in unique_clips_df.iterrows():       # for every unique clip...
+        for _, clip in merged_construct_dfs.iterrows():     # ...for every clip...
+            if unique_clip.equals(clip):                    # ...if they are the same
+                clip_count[i] = clip_count[i] + 1           # ...tally that unique clip
+    clip_count = clip_count // FINAL_ASSEMBLIES_PER_CLIP + 1    # find the number of clip wells needed (ie there is a max number of clip uses per reaction/well, if required clips exceeds this for a unique clip then start a new well)
+    clips_df['number'] = [int(i) for i in clip_count.tolist()]  # add a count column to unique clips df
 
-#     # Associate well/s for each CLIP reaction
-#     clips_df['mag_well'] = pd.Series(['0'] * len(clips_df.index),
-#                                      index=clips_df.index)
-#     for index, number in clips_df['number'].iteritems():
-#         if index == 0:
-#             mag_wells = []
-#             for x in range(number):
-#                 mag_wells.append(mplates.final_well(x + 1 + 48))
-#             clips_df.at[index, 'mag_well'] = tuple(mag_wells)
-#         else:
-#             mag_wells = []
-#             for x in range(number):
-#                 well_count = clips_df.loc[
-#                     :index - 1, 'number'].sum() + x + 1 + 48
-#                 mag_wells.append(mplates.final_well(well_count))
-#             clips_df.at[index, 'mag_well'] = tuple(mag_wells)
-#     return clips_df
-
-
-# def generate_sources_dict(paths):
-#     """Imports csvs files containing a series of parts/linkers with 
-#     corresponding information into a dictionary where the key corresponds with
-#     part/linker and the value contains a tuple of corresponding information.
-
-#     Args:
-#         paths (list): list of strings each corresponding to a path for a 
-#                       sources csv file. 
-
-#     """
-#     sources_dict = {}
-#     for deck_index, path in enumerate(paths):
-#         with open(path, 'r') as csvfile:
-#             csv_reader = csv.reader(csvfile)
-#             for index, source in enumerate(csv_reader):
-#                 if index != 0:
-#                     csv_values = source[1:]
-#                     csv_values.append(SOURCE_DECK_POS[deck_index])
-#                     sources_dict[str(source[0])] = tuple(csv_values)
-#     return sources_dict
+    # Associate well/s for each CLIP reaction
+    clips_df['mag_well'] = pd.Series(['0'] * len(clips_df.index),
+                                     index=clips_df.index)  # adds new column 'mag_well'
+    for index, number in clips_df['number'].iteritems():    # for every unique clip...
+        if index == 0:                                      # ...if its the first clip...
+            mag_wells = [] 
+            for x in range(number):                         # ...for every count of that clip...
+                mag_wells.append(mplates.final_well(x + 1 + 48))    # parse into mag wells to return list
+            clips_df.at[index, 'mag_well'] = tuple(mag_wells)       # return tuple to mag_well col
+        else:
+            mag_wells = []
+            for x in range(number):
+                well_count = clips_df.loc[
+                    :index - 1, 'number'].sum() + x + 1 + 48        # adds all previous clips to clip count
+                mag_wells.append(mplates.final_well(well_count))
+            clips_df.at[index, 'mag_well'] = tuple(mag_wells)
+    return clips_df
 
 
-# def generate_clips_dict(clips_df, sources_dict):
-#     """Using clips_df and sources_dict, returns a clips_dict which acts as the 
-#     sole variable for the opentrons script "clip.ot2.py".
+def generate_sources_dict(paths):
+    """Imports csvs files containing a series of parts/linkers with 
+    corresponding information into a dictionary where the key corresponds with
+    part/linker and the value contains a tuple of corresponding information.
 
-#     """
-#     max_part_vol = CLIP_VOL - (T4_BUFF_VOL + BSAI_VOL + T4_LIG_VOL
-#                                + CLIP_MAST_WATER + 2)
-#     clips_dict = {'prefixes_wells': [], 'prefixes_plates': [],
-#                   'suffixes_wells': [], 'suffixes_plates': [],
-#                   'parts_wells': [], 'parts_plates': [], 'parts_vols': [],
-#                   'water_vols': []}
+    Args:
+        paths (list): list of strings each corresponding to a path for a 
+                      sources csv file. 
 
-#     # Generate clips_dict from args
-#     try:
-#         for _, clip_info in clips_df.iterrows():
-#             prefix_linker = clip_info['prefixes']
-#             clips_dict['prefixes_wells'].append([sources_dict[prefix_linker][0]]
-#                                                 * clip_info['number'])
-#             clips_dict['prefixes_plates'].append(
-#                 [handle_2_columns(sources_dict[prefix_linker])[2]] * clip_info['number'])
-#             suffix_linker = clip_info['suffixes']
-#             clips_dict['suffixes_wells'].append([sources_dict[suffix_linker][0]]
-#                                                 * clip_info['number'])
-#             clips_dict['suffixes_plates'].append(
-#                 [handle_2_columns(sources_dict[suffix_linker])[2]] * clip_info['number'])
-#             part = clip_info['parts']
-#             clips_dict['parts_wells'].append([sources_dict[part][0]]
-#                                              * clip_info['number'])
-#             clips_dict['parts_plates'].append([handle_2_columns(sources_dict[part])[2]]
-#                                               * clip_info['number'])
-#             if not sources_dict[part][1]:
-#                 clips_dict['parts_vols'].append([DEFAULT_PART_VOL] *
-#                                                 clip_info['number'])
-#                 clips_dict['water_vols'].append([max_part_vol - DEFAULT_PART_VOL]
-#                                                 * clip_info['number'])
-#             else:
-#                 part_vol = round(
-#                     PART_PER_CLIP / float(sources_dict[part][1]), 1)
-#                 if part_vol < MIN_VOL:
-#                     part_vol = MIN_VOL
-#                 elif part_vol > max_part_vol:
-#                     part_vol = max_part_vol
-#                 water_vol = max_part_vol - part_vol
-#                 clips_dict['parts_vols'].append(
-#                     [part_vol] * clip_info['number'])
-#                 clips_dict['water_vols'].append(
-#                     [water_vol] * clip_info['number'])
-#     except KeyError:
-#         sys.exit('likely part/linker not listed in sources.csv')
-#     for key, value in clips_dict.items():
-#         clips_dict[key] = [item for sublist in value for item in sublist]
-#     return clips_dict
+    """
+    sources_dict = {}
+    for deck_index, path in enumerate(paths):   # deck_index allocated to each source
+        with open(path, 'r') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            for index, source in enumerate(csv_reader):
+                if index != 0:
+                    csv_values = source[1:]     # adds the part well location and concentration
+                    csv_values.append(SOURCE_DECK_POS[deck_index])      # references the available source deck locations, allocates to the deck index, adds this value to the tuple
+                    sources_dict[str(source[0])] = tuple(csv_values)    # adds value to sources dict
+    return sources_dict
 
 
-# def generate_final_assembly_dict(constructs_list, clips_df):
-#     """Using constructs_list and clips_df, returns a dictionary of final
-#     assemblies with keys defining destination plate well positions and values
-#     indicating which clip reaction wells are used.
+def generate_clips_dict(clips_df, sources_dict):
+    """Using clips_df and sources_dict, returns a clips_dict which acts as the 
+    sole variable for the opentrons script "clip.ot2.py".
 
-#     """
-#     final_assembly_dict = {}
-#     clips_count = np.zeros(len(clips_df.index))
+    """
+    max_part_vol = CLIP_VOL - (T4_BUFF_VOL + BSAI_VOL + T4_LIG_VOL
+                               + CLIP_MAST_WATER + 2)
+                               
+    clips_dict = {'prefixes_wells': [], 'prefixes_plates': [],
+                  'suffixes_wells': [], 'suffixes_plates': [],
+                  'parts_wells': [], 'parts_plates': [], 'parts_vols': [],
+                  'water_vols': []}
+
+    # Generate clips_dict from args
+    try:
+        for _, clip_info in clips_df.iterrows():                                            # for every unique clip...
+            prefix_linker = clip_info['prefixes']                                           # ...subset the prefix linker...
+            clips_dict['prefixes_wells'].append([sources_dict[prefix_linker][0]]            # ...add the well ID to the clips_dict prefix wells as many times as unique clip used
+                                                * clip_info['number'])
+            clips_dict['prefixes_plates'].append(                                           # ...pass the prefix linker info (well pos, conc, deck pos) to handle_2_columns(), returning tuple of 3 values in the same format
+                [handle_2_columns(sources_dict[prefix_linker])[2]] * clip_info['number'])
+            suffix_linker = clip_info['suffixes']                                           # ...subset the prefix linker...
+            clips_dict['suffixes_wells'].append([sources_dict[suffix_linker][0]]            # ...add the well ID to the clips_dict suffix wells as many times as unique clip used
+                                                * clip_info['number'])
+            clips_dict['suffixes_plates'].append(                                           # ...return suffix linker info in correct format as before
+                [handle_2_columns(sources_dict[suffix_linker])[2]] * clip_info['number'])
+            part = clip_info['parts']                                                       # ...subset the part...
+            clips_dict['parts_wells'].append(                                               # ...pass the part info (well pos, conc, deck pos) to handle_2_columns(), returning tuple of 3 values in the same format
+                [sources_dict[part][0]] * clip_info['number'])
+            clips_dict['parts_plates'].append(                                              # ...return part info in correct format as before
+                [handle_2_columns(sources_dict[part])[2]] * clip_info['number'])
+            if not sources_dict[part][1]:                                                   # if concentration not defined...
+                clips_dict['parts_vols'].append([DEFAULT_PART_VOL] *                        # ...add the default vol required * number of clips required
+                                                clip_info['number'])
+                clips_dict['water_vols'].append([max_part_vol - DEFAULT_PART_VOL]           # ...add the water vol required * number of clips required
+                                                * clip_info['number'])
+            else:
+                part_vol = round(
+                    PART_PER_CLIP / float(sources_dict[part][1]), 1)    # c1*v1/c2 = v2 where parts normally require 1ul at 100ng/ul 
+                if part_vol < MIN_VOL:                                  # correct for min vol requirement (could add min conc here instead)
+                    part_vol = MIN_VOL
+                elif part_vol > max_part_vol:                           # correct for max vol requirement
+                    part_vol = max_part_vol
+                water_vol = max_part_vol - part_vol                     # calculate water vol
+                clips_dict['parts_vols'].append(
+                    [part_vol] * clip_info['number'])
+                clips_dict['water_vols'].append(
+                    [water_vol] * clip_info['number'])
+    except KeyError:
+        sys.exit('likely part/linker not listed in sources.csv')
+    for key, value in clips_dict.items():                               # removes sublists from dict values
+        clips_dict[key] = [item for sublist in value for item in sublist]
+    return clips_dict
+
+
+def generate_final_assembly_dict(constructs_list, clips_df):
+    """Using constructs_list and clips_df, returns a dictionary of final
+    assemblies with keys defining destination plate well positions and values
+    indicating which clip reaction wells are used.
+
+    """
+    final_assembly_dict = {}
+    clips_count = np.zeros(len(clips_df.index))
 #     for construct_index, construct_df in enumerate(constructs_list):
 #         construct_well_list = []
 #         for _, clip in construct_df.iterrows():
@@ -569,29 +569,24 @@ def generate_clips_df(constructs_list):
 #             value.to_csv(csvfile, index=index)
 #             csvwriter.writerow('')
 
-# def handle_2_columns(datalist):
-#     """This function has the intent of changing:
-#     ('A8', '2') => ('A8', '', '2')
-#     ('A8', '', '2') => ('A8', '', '2')
-#     [('E2', '5')] => [('E2', '', '5')]
-#     [('G1', '', '5')] => [('G1', '', '5')]
-#     with the purpose of handling 2 column csv part file inputs,
-#     as at times when 2 column csv files are input it creates tuples
-#     of length 2 instead of 3
-#     """
-#     return_list = 0
-#     if isinstance(datalist,list):
-#         datalist = datalist[0]
-#         return_list = 1
-#     if len(datalist) == 2:
-#         datalist = list(datalist)
-#         datalist.insert(1,"")
-#         datalist = tuple(datalist)
-#     if return_list:
-#         mylist = [""]
-#         mylist[0] = datalist
-#         return mylist
-#     return datalist
+def handle_2_columns(datalist, return_list = False):
+    """This function has the intent of changing:
+    ('A8', '2') => ('A8', '', '2')
+    ('A8', '', '2') => ('A8', '', '2')
+    [('E2', '5')] => [('E2', '', '5')]
+    [('G1', '', '5')] => [('G1', '', '5')]
+    with the purpose of handling 2 column csv part file inputs,
+    as at times when 2 column csv files are input it creates tuples
+    of length 2 instead of 3
+    """
+    if isinstance(datalist,list):   # if data list is a list, extract tuple
+        datalist = datalist[0] 
+        return_list = True
+    if len(datalist) == 2:          # if tuple is of length 2, insert empty value at position 1
+        datalist = (datalist[0], "", datalist[1])
+    if return_list:                 # if list passed to function, return output as list
+        datalist = [datalist]
+    return datalist
 
 if __name__ == '__main__':
     main()
--- dnabot/mplates.py
+++ dnabot/mplates.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu May 30 17:05:37 2019
+Created on Sat Feb 27 23:20:37 2021
 
-@author: mh2210
+@author: ljh119
 """
 
-def final_well(sample_number):
+def final_well(n):
     """Determines well containing the final sample from sample number.
     
     """
-    letter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-    final_well_column = sample_number // 8 + \
-        (1 if sample_number % 8 > 0 else 0)
-    final_well_row = letter[sample_number - (final_well_column - 1) * 8 - 1]
-    return final_well_row + str(final_well_column)
+    letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+    row = letters[n%8-1]
+    col = (n-1)//8 + 1
+
+    return row + str(col)
